49dc96381bfc94c96995a5ec263c055faa566bf9
alimovj <youremail@example.com>
2025-08-08 12:39:37 +0500

    install Laravel Sanctum for API auth

--- app/Console/Commands/CleanUnverifiedUsersCommand.php
+++ app/Console/Commands/CleanUnverifiedUsersCommand.php
@@ -6,7 +6,7 @@ use App\Services\AuthService;
 
 class CleanUnverifiedUsersCommand extends Command
 {
-    protected $signature = 'users:clean-unverified';qqqqqqqqqqqqqqqqqqqqqqqqqqqqq
+    protected $signature = 'users:clean-unverified';qqqqqqqqqqqqqqqqqqqqqqqqqqqq
     protected $description = 'Deletes users who are not verified within 3 days';
 
     protected AuthService $authService;
