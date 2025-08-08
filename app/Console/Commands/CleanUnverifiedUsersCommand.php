d72038d77bea80d21d9b7630a6dae08df8e5ef3a
alimovj <youremail@example.com>
2025-08-08 12:42:49 +0500

    handle email already exists error in register

--- app/Console/Commands/CleanUnverifiedUsersCommand.php
+++ app/Console/Commands/CleanUnverifiedUsersCommand.php
@@ -6,7 +6,7 @@ use App\Services\AuthService;
 
 class CleanUnverifiedUsersCommand extends Command
 {
-    protected $signature = 'users:clean-unverified';qqqqqqqqqqqqqqqqq
+    protected $signature = 'users:clean-unverified';qqqqqqqqqqqqqqqq
     protected $description = 'Deletes users who are not verified within 3 days';
 
     protected AuthService $authService;
