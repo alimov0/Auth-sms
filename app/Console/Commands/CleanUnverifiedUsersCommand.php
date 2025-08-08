b816941e20f91651bea14569ee457875e87da8c8
alimovj <youremail@example.com>
2025-08-08 12:41:42 +0500

    create AuthController with register and login methods

--- app/Console/Commands/CleanUnverifiedUsersCommand.php
+++ app/Console/Commands/CleanUnverifiedUsersCommand.php
@@ -6,7 +6,7 @@ use App\Services\AuthService;
 
 class CleanUnverifiedUsersCommand extends Command
 {
-    protected $signature = 'users:clean-unverified';qqqqqqqqqqqqqqqqqqqqq
+    protected $signature = 'users:clean-unverified';qqqqqqqqqqqqqqqqqqqq
     protected $description = 'Deletes users who are not verified within 3 days';
 
     protected AuthService $authService;
