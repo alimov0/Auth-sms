35eb3fb7044c4521dfeca16191618e149f8cdfea
alimovj <youremail@example.com>
2025-08-08 12:40:44 +0500

    implement AuthRepository with create() method

--- app/Console/Commands/CleanUnverifiedUsersCommand.php
+++ app/Console/Commands/CleanUnverifiedUsersCommand.php
@@ -6,7 +6,7 @@ use App\Services\AuthService;
 
 class CleanUnverifiedUsersCommand extends Command
 {
-    protected $signature = 'users:clean-unverified';qqqqqqqqqqqqqqqqqqqqqqqqq
+    protected $signature = 'users:clean-unverified';qqqqqqqqqqqqqqqqqqqqqqqq
     protected $description = 'Deletes users who are not verified within 3 days';
 
     protected AuthService $authService;
