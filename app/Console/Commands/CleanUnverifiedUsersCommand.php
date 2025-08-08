f25d181be332eff78b517531b264b29602d7b855
alimovj <youremail@example.com>
2025-08-08 12:37:35 +0500

    remove default Laravel welcome view

--- app/Console/Commands/CleanUnverifiedUsersCommand.php
+++ app/Console/Commands/CleanUnverifiedUsersCommand.php
@@ -6,7 +6,7 @@ use App\Services\AuthService;
 
 class CleanUnverifiedUsersCommand extends Command
 {
-    protected $signature = 'users:clean-unverified';
+    protected $signature = 'users:clean-unverified'
     protected $description = 'Deletes users who are not verified within 3 days';
 
     protected AuthService $authService;
